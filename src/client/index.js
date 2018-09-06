9b6445e191fb638918682e23363fe9da04679ed4
Bundit Nuntates <silkyland@gmail.com>
2018-09-06 16:17:20 +0700

    update

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,11 @@
 import ApolloClient from "apollo-boost";
 import config from "../config";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
+const cache = new InMemoryCache();
 const client = new ApolloClient({
-  uri: config.graphql.clientUrl
+  uri: config.graphql.clientUrl,
+  cache
 });
 
 export default client;
