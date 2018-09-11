fadde70fddfafec02c6ecdf90fdfdc1a6dd175cd
Bundit Nuntates <silkyland@gmail.com>
2018-09-11 11:52:18 +0700

    edit fix

--- src/query/user.js
+++ src/query/user.js
@@ -4,11 +4,46 @@ export const GET_USERS = gql`
   {
     users {
       id
+      userType {
+        id
+        name
+      }
       name
       username
       email
       createdAt
       updatedAt
+    }
+  }
+`;
+
+export const UPDATE_USER = gql`
+  mutation updateUser(
+    $id: ID
+    $userTypeId: Int
+    $name: String
+    $username: String
+    $email: String
+  ) {
+    updateUser(id: $id, name: $name, username: $username, email: $email) {
+      id
+      userType {
+        id
+        name
+      }
+      name
+      username
+      email
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const DELETE_USER = gql`
+  mutation deleteUser($id: ID!) {
+    deleteUser(id: $id) {
+      id
     }
   }
 `;
